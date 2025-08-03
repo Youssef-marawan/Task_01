776d1461877093d89e5bdfe61069ab1d3df6511f
Lenovo <Lenovo@DESKTOP-G2Q8QH2>
2025-08-03 03:37:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Welcome to Islam's Carpet Cleaning Service");
+Console.WriteLine("Enter Number of small carpets:");
+int small = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Number of large carpets:");
+int large = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Price per small room: 25$");
+Console.WriteLine("Price per large room: 35$");
+int cost = (small * 25) + (large * 35);
+double tax = cost * 0.06;
+Console.WriteLine($"Cost : {cost} $");
+Console.WriteLine($"Tax  : {tax} $");
+Console.WriteLine("=========================");
+double total = cost + tax;
+Console.WriteLine($"Total estimate : {total} $");
+Console.WriteLine("This estimate is valid for 30 days");
+
